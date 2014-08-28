80c1fe05683fb9ab84a514c47ff2b2f6f635b99f
Alex Crowe <alex@pancentric.com>
2014-08-28 16:12:48 +0100

    removed unneeded acceptance tests

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -6,21 +6,12 @@ describe 'confd class' do
     # Using puppet_apply as a helper
     it 'should work with no errors' do
       pp = <<-EOS
-      class { 'conf': }
+      class { 'confd': }
       EOS
 
       # Run it twice and test for idempotency
       expect(apply_manifest(pp).exit_code).to_not eq(1)
       expect(apply_manifest(pp).exit_code).to eq(0)
-    end
-
-    describe package('confd') do
-      it { should be_installed }
-    end
-
-    describe service('confd') do
-      it { should be_enabled }
-      it { should be_running }
     end
   end
 end
