8edb16a9996f90bd472ab56a60dc8175341b108f
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-01-25 10:42:32 -0500

    quick upstart support

--- manifests/install.pp
+++ manifests/install.pp
@@ -12,4 +12,19 @@ class confd::install {
     mode   => '0750',
     source => $binary_src
   }
+
+  # XXX: This is hard-coded upstart stuff
+  file { '/etc/init/confd.conf':
+    mode    => '0444',
+    owner   => 'root',
+    group   => 'root',
+    content => template('confd/confd.upstart.erb'),
+  }
+  file { '/etc/init.d/confd':
+    ensure => link,
+    target => '/lib/init/upstart-job',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
 }
