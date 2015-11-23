4254a001cc7bd3d0f3097fbe5b330a9a45a7122e
Alex Crowe <alex.j.crowe@googlemail.com>
2015-11-23 22:11:45 +0000

    added deprcation notify for debug, quiet, verbose

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,17 +35,30 @@ class confd(
   validate_hash($resources)
 
   if $backend { validate_re($backend, ['^etcd$', '^consul$', '^zookeeper$', '^dynamodb$', '^redis$', '^env$']) }
-  if $debug { validate_bool($debug) }
   if $interval { validate_re($interval, '^\d+') }
   if $confdnoop { validate_bool($confdnoop) }
   if $log_level { validate_re($log_level, ['^debug$', '^info$', '^warn(ing)?$', '^error$', '^fatal$', '^panic$']) }
   if $nodes { validate_array($nodes) }
   if $prefix { validate_string($prefix) }
-  if $quiet { validate_bool($quiet) }
   if $scheme { validate_re($scheme, '^https?$') }
   if $srv_domain { validate_string($srv_domain) }
-  if $verbose { validate_bool($verbose) }
   if $watch { validate_bool($watch) }
+
+  $deprecated = '[confd] *** DEPRECATED ***: $debug, $quiet, $verbose have been replaced by $log_level'
+
+  if $debug {
+    validate_bool($debug)
+    notify {$deprecated:}
+  }
+  if $quiet {
+    validate_bool($quiet)
+    notify {$deprecated:}
+  }
+  if $verbose {
+    validate_bool($verbose)
+    notify {$deprecated:}
+  }
+
   if $client_cakeys {
     validate_string($client_cakeys)
     $client_cakeys_path = "${confdir}/ssl/${client_cakeys}"
